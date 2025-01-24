ef9444a5ab483c4e93baf3da1cb9000c20c90ff4
xlezzerx <151861662+xlezzerx@users.noreply.github.com>
2025-01-24 12:21:30 -0500

    fixed package.json file with the dependencies and ran build

---
+++ docs/service-worker.7763843c.js
@@ -0,0 +1,2 @@
+const e=["/CheckList/","/CheckList/index.html","/CheckList/style.css","/CheckList/app.js","/CheckList/manifest.json","/CheckList/icons/icon-128.png","/CheckList/icons/icon-512.png"];self.addEventListener("install",t=>{t.waitUntil(caches.open("to-do-pwa-cache-v1").then(t=>t.addAll(e)))}),self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(t=>t||fetch(e.request)))});
+//# sourceMappingURL=service-worker.7763843c.js.map
